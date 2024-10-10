29ca6accb45dbc40f22d8eb4931e790514f8b607
Antonio <antonicut@gmail.com>
2024-10-10 14:41:20 +0200

    add - 01-foundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Asegúrate de definir el subdirectorio correcto en "base"
+  //base: 'https:/devtalles.antonydev.tech/01-foundation/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto
+});
+
+
+
+
+
+
+
+
+
