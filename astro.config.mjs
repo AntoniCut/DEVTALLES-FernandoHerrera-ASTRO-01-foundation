3c3b45d845264f1ba2d87c891642acec2ba7c167
Antonio <antonicut@gmail.com>
2024-10-11 20:58:39 +0200

    Add - Sección 2 - Introducción a Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   // Asegúrate de definir el subdirectorio correcto en "base"
-  //base: 'https:/devtalles.antonydev.tech/01-foundation/',  // <--- Este es el subdirectorio donde se encuentra tu proyecto
+  site: 'https://devtalles.antonydev.tech/astro/01-foundation',
+  //base: '/astro/01-foundation/'  // <--- Este es el subdirectorio donde se encuentra tu proyecto -----  Descomentar para Producción  -----
 });
+
 
 
 
